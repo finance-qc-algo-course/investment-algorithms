1f34c74b495a55f49e82623f0ee9d28ddecbf1fb
Arman Urazgulov <urazgulov.a@phystech.edu>
2022-05-03 10:43:32 +0300

    modified NDR

--- NDR_sample.py
+++ NDR_sample.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import warnings
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.decomposition import NMF as NMF_
@@ -41,7 +42,6 @@ class NDR(TransformerMixin, BaseEstimator):
         """
         fit
         """
-        # to do
         if self.is_NPCA:
             self._NPCA_dim_red(X, self.n_comp, self.window_size)
         
@@ -55,7 +55,13 @@ class NDR(TransformerMixin, BaseEstimator):
         """
         transform
         """
-        return self._transform(X)
+        if self.is_NPCA:
+            return self._NPCA_dim_red(X, self.n_comp, self.window_size)
+        
+        if self.is_NMF:
+            return self._NMF_dim_red(X, self.n_comp, self.window_size)
+        
+        return X.pct_change(1).fillna(0)
     
     
     def fit_transform(self, X, y=None):
@@ -69,63 +75,74 @@ class NDR(TransformerMixin, BaseEstimator):
         """
         _transform
         """
-        # to do
         if self.is_NPCA:
             return self.npca_returns
         
         if self.is_NMF:
-            return self.nmf_X
+            return self.nmf_returns
+        
+        return X
         
     
     def score(self, X, y=None):
-        #to do
         if self.is_NPCA:
-            return 0
+            return np.mean(self.expl_var)
         
         if self.is_NMF:
             return -np.mean(self.norm_list)
+        
+        return 0
     
     
     def _NPCA_dim_red(self, X, n_comp, window_size):
         components_ind = 6
         new_prices = list()
+        expl_var_list = list()
 
         for i in range(0, len(X.index), window_size):
-            var = nsprcomp.nsprcomp(X[i : i + window_size].T, ncomp=n_comp, center=False, scale=True, nneg=True)
-            new_prices.append(self._SVP(var[components_ind].T))
+            obj = nsprcomp.nsprcomp(X[i : i + window_size].T, ncomp=n_comp, center=False, scale=False, nneg=True)
+            new_prices.append(self._SVP(obj[components_ind].T))
+            variance = sum(np.var(X[i : i + window_size].T))
+            expl_var = sum(obj[0]**2 / variance)
+            expl_var_list.append(expl_var)
 
         new_prices = np.vstack(new_prices)
+        expl_var = np.vstack(expl_var_list)
         df_new_prices = pd.DataFrame(data=new_prices, index=np.arange(0, len(new_prices)), columns=X.columns)
         new_returns = df_new_prices.pct_change(1).fillna(0)
         new_returns = new_returns.drop(new_returns.index[0])
 
         self.npca_returns = new_returns
         self.npca_prices = new_prices
+        self.expl_var = expl_var
 
         return new_returns
     
     
     def _NMF_dim_red(self, X, n_comp, window_size):
-        new_prices = list()
-        norm_list = list()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            
+            new_prices = list()
+            norm_list = list()
 
-        for i in range(0, len(X.index), window_size):
-            model = NMF_(n_components=n_comp)
-            W = model.fit_transform(X[i : i + window_size].T)
-            H = model.components_
-            new_prices.append(self._SVP(W.T))
-            norm_list.append(np.linalg.norm(W @ H - X[i : i + window_size].T))
+            for i in range(0, len(X.index), window_size):
+                model = NMF_(n_components=n_comp)
+                W = model.fit_transform(X[i : i + window_size].T)
+                H = model.components_
+                new_prices.append(self._SVP(W.T))
+                norm_list.append(np.linalg.norm(W @ H - X[i : i + window_size].T))
+
+            new_prices = np.vstack(new_prices)
+            df_new_prices = pd.DataFrame(data=new_prices, index=np.arange(0, len(new_prices)), columns=X.columns)
+            new_returns = df_new_prices.pct_change(1).fillna(0)
+            new_returns = new_returns.drop(new_returns.index[0])
+
+            self.norm_list = norm_list
+            self.nmf_returns = new_returns
+            self.nmf_prices = new_prices
         
-        new_prices = np.vstack(new_prices)
-        df_new_prices = pd.DataFrame(data=new_prices, index=np.arange(0, len(new_prices)), columns=X.columns)
-        new_returns = df_new_prices.pct_change(1).fillna(0)
-        new_returns = new_returns.drop(new_returns.index[0])
-        
-        self.norm_list = norm_list
-        self.nmf_returns = new_returns
-        self.nmf_prices = new_prices
-        
-        return new_returns, self.norm_list
+        return self.nmf_returns, self.norm_list
     
     
     def _SVP(self, components):
