2223fa95ee647204b259dffd86a496ab97459066
Viktor Khaymonenko <khaimonenko.vb@phystech.edu>
2022-12-21 13:23:14 +0300

    Some changes

--- InvestingFramework/modules/model/wrappers/SimpleIndicatorsModel.py
+++ InvestingFramework/modules/model/wrappers/SimpleIndicatorsModel.py
@@ -1,8 +1,8 @@
 from backtesting.lib import SignalStrategy
+from catboost import CatBoostRegressor
 
 from ..base import Model
 
 class SimpleIndicatorsModel(Model):
     def __init__(self, train_data):
-        pass
-        
+        self.model = CatBoostRegressor()
